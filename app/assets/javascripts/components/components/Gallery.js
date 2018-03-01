233280dcc09693eb648684f02101c3ffde6a72d5
Vishal Jagtap <jagtap.vishal30@gmail.com>
2018-03-01 14:30:02 +0530

    Added Photo Grid

--- app/assets/javascripts/components/components/Gallery.js
+++ app/assets/javascripts/components/components/Gallery.js
@@ -15,6 +15,94 @@ const Gallery = () => (
                 </figure>
             </div>
         </div>
+
+        <br></br> <br></br>
+        <ul id="rig">
+            <li>
+                <a class="rig-cell">
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Lorem Ipsum Dolor</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Consectetur Adipiscing</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Vivamus ut Felis</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Curabitur tempus</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Sed et nunc luctus</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Suspendisse tellus</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">pellentesque odio</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Nam euismod</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Vestibulum ultricies</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">quis interdum sapien</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Maecenas vitae</span>
+                </a>
+            </li>
+            <li>
+                <a class="rig-cell"  >
+                    <img class="rig-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg" />
+                    <span class="rig-overlay"></span>
+                    <span class="rig-text">Proin Scelerisque</span>
+                </a>
+            </li>
+        </ul>
     </div>
 );
 
